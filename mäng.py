d134c8c2e15515a37756f29bc80dca3428f0591b
andrusmuur <44435372+andrusmuur@users.noreply.github.com>
2018-12-12 21:34:32 +0200

    Add files via upload

--- mäng.py
+++ mäng.py
@@ -16,14 +16,18 @@ class MyGame(arcade.Window):
         
         self.view_bottom = 0
         self.view_left = 0
-        
+        self.score = 0
         self.game_over = False
+        
+        self.wall_x = 800
+        self.last_obstacle_x = 1000
         
     def setup(self):
         
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
         self.obstacle_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
         
         self.player_sprite = arcade.Sprite("pildid/main_character.png", 1)
         self.player_sprite.center_x = 400
@@ -40,24 +44,10 @@ class MyGame(arcade.Window):
             
             self.wall_sprite = arcade.Sprite("pildid/põrand.png", 0.1)
             self.wall_sprite.center_x = floor_x
-            self.wall_sprite.center_y = 600
+            self.wall_sprite.center_y = 580
             self.wall_list.append(self.wall_sprite)
             floor_number += 1
             floor_x += 45
-
-#            if floor_number % 10 == 0:
-#                obstacle_x = floor_x * random.randint(1, 3)
-#                if obstacle_x < 1000:
-#                    continue
-#                else:
-#                    self.obstacle_sprite = arcade.Sprite("pildid/spike.png", 0.3)
-#                    self.obstacle_sprite.center_x = obstacle_x
-#                    self.obstacle_sprite.center_y = 70
-#                    self.obstacle_list.append(self.obstacle_sprite)
-
-            
-            
-            
             
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, gravity_constant = 2)
         self.player_sprite.change_x = player_speed
@@ -71,6 +61,7 @@ class MyGame(arcade.Window):
         self.player_list.draw()
         self.wall_list.draw()
         self.obstacle_list.draw()
+        self.coin_list.draw()
         
     def update(self, delta_time):
         changed = False
@@ -81,28 +72,54 @@ class MyGame(arcade.Window):
         use_spatial_hash=False
         if changed:
             arcade.set_viewport(self.view_left, screen_width + self.view_left, self.view_bottom, screen_height + self.view_bottom)
+            self.wall_sprite = arcade.Sprite("pildid/põrand.png", 0.1)
+            self.wall_sprite.center_x = self.wall_x
+            self.wall_sprite.center_y = 25
+            if self.wall_x - self.player_sprite.center_x < 500:
+                self.wall_list.append(self.wall_sprite)
             
             self.wall_sprite = arcade.Sprite("pildid/põrand.png", 0.1)
-            self.wall_sprite.center_x = round(self.view_left + screen_width)
-            self.wall_sprite.center_y = 25
-            if self.wall_sprite.center_x % 8 == 0:
+            self.wall_sprite.center_x = self.wall_x
+            self.wall_sprite.center_y = 580
+            if self.wall_x - self.player_sprite.center_x < 500:
                 self.wall_list.append(self.wall_sprite)
-                if self.wall_sprite.center_x % 24 == 0:
-                    self.wall_list[0].kill()
-                    self.wall_list[0].kill()
-                    self.wall_list[0].kill()
-                    self.wall_list[0].kill()
-                    self.wall_list[0].kill()
+                self.wall_x += 45
+            
+            if self.wall_x % 50 == 0:
+                self.obstacle_sprite = arcade.Sprite("pildid/spike.png", 0.3)
+                self.obstacle_sprite.center_x = self.last_obstacle_x + random.randint(300, 600)
+                self.obstacle_sprite.center_y = 72
+                self.obstacle_list.append(self.obstacle_sprite)
+                self.coin_sprite = arcade.Sprite("pildid/coin.png", 0.08)
+                self.coin_sprite.center_x = self.last_obstacle_x + 50
+                self.coin_sprite.center_y = 70
+                for x in random.sample(range(self.coin_sprite.center_x, self.obstacle_sprite.center_x - 50), 1):
+                    self.coin_sprite.center_x = x
+                    self.coin_list.append(self.coin_sprite)
+                self.last_obstacle_x = self.obstacle_sprite.center_x
+                    
                 
-            self.wall_sprite = arcade.Sprite("pildid/põrand.png", 0.1)
-            self.wall_sprite.center_x = round(self.view_left + screen_width)
-            self.wall_sprite.center_y = 600
-            if self.wall_sprite.center_x % 8 == 0:
-                self.wall_list.append(self.wall_sprite)     
+        for wall in self.wall_list:
+            if self.player_sprite.center_x > wall.center_x + 400:
+                self.wall_list[0].kill()
+                
+        for obstacle in self.obstacle_list:
+            if self.player_sprite.center_x > obstacle.center_x + 600:
+                self.obstacle_list[0].kill()
+                
+        for coin in self.coin_list:
+           if self.player_sprite.center_x > coin.center_x + 600:
+                self.coin_list[0].kill()
+        
+        coins_hit = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+        for coin in coins_hit:
+            coin.kill()
+            self.score += 1
             
         if not self.game_over:
             self.obstacle_list.update()
             self.wall_list.update()
+            self.coin_list.update()
             
             if len(arcade.check_for_collision_with_list(self.player_sprite, self.obstacle_list)) > 0:
                 self.game_over = True
